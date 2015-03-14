0608a662535f5793bc705ab030d7a49ff860ceb2
Carmen <Carmen@Laptop>
2015-03-14 21:46:19 +0000

    All text styling completed.

--- public_html/javascript/gameDesignFilter.js
+++ public_html/javascript/gameDesignFilter.js
@@ -1,13 +1,22 @@
 function filterGames(type){
     if(type == 0){
+        $('#all').addClass("selected");
+        $('#analog').removeClass("selected");
+        $('#digital').removeClass("selected");
         $('.digitalGames').removeClass("hide");
         $('.analogGames').removeClass("hide");    
     }
     else if(type == 1){
+        $('#all').removeClass("selected");
+        $('#analog').addClass("selected");
+        $('#digital').removeClass("selected");
         $('.digitalGames').addClass("hide");
         $('.analogGames').removeClass("hide");
     }
     else{
+        $('#all').removeClass("selected");
+        $('#analog').removeClass("selected");
+        $('#digital').addClass("selected");
         $('.analogGames').addClass("hide");
         $('.digitalGames').removeClass("hide");
     }
--- public_html/javascript/visualMediaFilter.js
+++ public_html/javascript/visualMediaFilter.js
@@ -1,29 +1,54 @@
 function filterVisualMedia(type){
     if(type == 0){
+        $('#all').addClass("selected");
+        $('#photo').removeClass("selected");
+        $('#video').removeClass("selected");
+        $('#web').removeClass("selected");
+        $('#graphic').removeClass("selected");
         $('.photography').removeClass("hide");
         $('.videography').removeClass("hide"); 
         $('.webDesign').removeClass("hide");
         $('.graphicDesign').removeClass("hide"); 
     }
     else if(type == 1){
+        $('#all').removeClass("selected");
+        $('#photo').addClass("selected");
+        $('#video').removeClass("selected");
+        $('#web').removeClass("selected");
+        $('#graphic').removeClass("selected");
         $('.photography').removeClass("hide");
         $('.videography').addClass("hide"); 
         $('.webDesign').addClass("hide");
         $('.graphicDesign').addClass("hide"); 
     }
     else if(type == 2){
+        $('#all').removeClass("selected");
+        $('#photo').removeClass("selected");
+        $('#video').addClass("selected");
+        $('#web').removeClass("selected");
+        $('#graphic').removeClass("selected");
         $('.photography').addClass("hide");
         $('.videography').removeClass("hide"); 
         $('.webDesign').addClass("hide");
         $('.graphicDesign').addClass("hide"); 
     }
     else if(type == 3){
+        $('#all').removeClass("selected");
+        $('#photo').removeClass("selected");
+        $('#video').removeClass("selected");
+        $('#web').addClass("selected");
+        $('#graphic').removeClass("selected");
         $('.photography').addClass("hide");
         $('.videography').addClass("hide"); 
         $('.webDesign').removeClass("hide");
         $('.graphicDesign').addClass("hide");
     }
     else{
+        $('#all').removeClass("selected");
+        $('#photo').removeClass("selected");
+        $('#video').removeClass("selected");
+        $('#web').removeClass("selected");
+        $('#graphic').addClass("selected");
         $('.photography').addClass("hide");
         $('.videography').addClass("hide"); 
         $('.webDesign').addClass("hide");
